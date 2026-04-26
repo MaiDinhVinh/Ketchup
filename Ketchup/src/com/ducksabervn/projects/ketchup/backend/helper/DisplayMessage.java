aa69b54121b36342bfda1ef6e59b71304fbb9651
DucksaberVN <business@ducksabervn.com>
2026-04-26 22:06:34 +0700

    Update DisplayMessage.java

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/helper/DisplayMessage.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/helper/DisplayMessage.java
@@ -1,6 +1,8 @@
 package com.ducksabervn.projects.ketchup.backend.helper;
 
 import javax.swing.*;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public final class DisplayMessage {
     public static void displayError(JFrame parentComponent, String message){
@@ -10,5 +12,22 @@ public final class DisplayMessage {
                 "Ketchup",
                 JOptionPane.ERROR_MESSAGE
         );
+    }
+
+    public static boolean displayConfirmationDialog(JFrame mainFrame,
+                                                 String message,
+                                                 String title,
+                                                 int optionPane){
+        return JOptionPane.showConfirmDialog(mainFrame,
+                message,
+                title,
+                optionPane) == JOptionPane.YES_OPTION;
+    }
+
+    public static void displayWarning(JFrame mainFrame,
+                                      String message,
+                                      String title,
+                                      int optionPane){
+        JOptionPane.showMessageDialog(mainFrame, message, title, optionPane);
     }
 }
