bb3750f87b3985d4d2026c6547f781f1b50783d7
Nguyen Dinh Quy <25quy.nd@vinuni.edu.vn>
2026-04-26 23:25:56 +0700

    Update Credential.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/credientials/Credential.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/credientials/Credential.java
@@ -3,14 +3,20 @@ package com.ducksabervn.projects.ketchup.backend.credientials;
 import java.util.TreeMap;
 
 public class Credential {
+    private String username;
     private String email;
     private String password;
     private boolean isAdmin;
 
-    public Credential(String email, String password, boolean isAdmin){
+    public Credential(String username, String email, String password, boolean isAdmin){
+        this.username = username;
         this.email = email;
         this.password = password;
         this.isAdmin = isAdmin;
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public boolean isAdmin() {
