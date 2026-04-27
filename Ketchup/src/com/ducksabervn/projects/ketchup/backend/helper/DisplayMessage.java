ca76579579e0d11a642a01cf647dd8a985c702b6
Tran Phan Anh <25anh.tp@vinuni.edu.vn>
2026-04-27 10:18:21 +0700

    Update DisplayMessage.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/helper/DisplayMessage.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/helper/DisplayMessage.java
@@ -1,35 +1,36 @@
 package com.ducksabervn.projects.ketchup.backend.helper;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
 public final class DisplayMessage {
-    public static void displayError(JFrame parentComponent, String message){
+    public static void displayError(Component component, String message){
         JOptionPane.showMessageDialog(
-                parentComponent,
+                component,
                 message,
                 "Ketchup",
                 JOptionPane.ERROR_MESSAGE
         );
     }
 
-    public static boolean displayConfirmationDialog(JFrame mainFrame,
-                                                 String message,
-                                                 String title){
-        return JOptionPane.showConfirmDialog(mainFrame,
+    public static boolean displayConfirmationDialog(Component component,
+                                                    String message,
+                                                    String title){
+        return JOptionPane.showConfirmDialog(component,
                 message,
                 title,
                 JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
     }
 
-    public static void displayWarning(JFrame mainFrame,
+    public static void displayWarning(Component component,
                                       String message){
-        JOptionPane.showMessageDialog(mainFrame, message, "Ketchup", JOptionPane.WARNING_MESSAGE);
+        JOptionPane.showMessageDialog(component, message, "Ketchup", JOptionPane.WARNING_MESSAGE);
     }
 
-    public static void displayInformation(JFrame mainFrame,
+    public static void displayInformation(Component component,
                                           String message){
-        JOptionPane.showMessageDialog(mainFrame, message, "Ketchup", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(component, message, "Ketchup", JOptionPane.INFORMATION_MESSAGE);
     }
 }
