ff675fae44915058665d7fb8577596a6f43dc561
Tran Phan Anh <25anh.tp@vinuni.edu.vn>
2026-05-08 17:41:08 +0700

    Update KetchupMain.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/main/KetchupMain.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/main/KetchupMain.java
@@ -1,8 +1,10 @@
 package com.ducksabervn.projects.ketchup.main;
 
-import com.ducksabervn.projects.ketchup.backend.admin.MovieRepository;
-import com.ducksabervn.projects.ketchup.backend.credientials.CredentialRepository;
-import com.ducksabervn.projects.ketchup.backend.helper.ReadCSVFile;
+import com.ducksabervn.projects.ketchup.backend.io.CredentialCsvIO;
+import com.ducksabervn.projects.ketchup.backend.movie.MovieRepository;
+import com.ducksabervn.projects.ketchup.backend.auth.CredentialRepository;
+import com.ducksabervn.projects.ketchup.backend.io.FileSystemInitializer;
+import com.ducksabervn.projects.ketchup.backend.io.MovieCsvIO;
 import com.ducksabervn.projects.ketchup.frontend.LoginUI;
 
 import javax.swing.*;
@@ -14,9 +16,9 @@ public class KetchupMain {
         }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e){
             e.printStackTrace();
         }
-        ReadCSVFile.initalize();
-        MovieRepository.setMovies(ReadCSVFile.readMoviesCsv());
-        CredentialRepository.setCredentials(ReadCSVFile.readUserCredentialsCsv());
+        FileSystemInitializer.initalize();
+        MovieRepository.setMovies(MovieCsvIO.getIO().readCsvFile());
+        CredentialRepository.setCredentials(CredentialCsvIO.getIO().readCsvFile());
         SwingUtilities.invokeLater(LoginUI::initialize);
     }
 }
