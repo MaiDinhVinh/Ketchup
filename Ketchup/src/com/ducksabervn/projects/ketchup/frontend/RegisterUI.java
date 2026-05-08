15198b12d788543224de2752cab0649a77dbe01f
Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>
2026-05-08 17:41:02 +0700

    Update RegisterUI.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/frontend/RegisterUI.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/frontend/RegisterUI.java
@@ -1,8 +1,8 @@
 package com.ducksabervn.projects.ketchup.frontend;
 
-import com.ducksabervn.projects.ketchup.backend.credientials.Credential;
-import com.ducksabervn.projects.ketchup.backend.credientials.CredentialRepository;
-import com.ducksabervn.projects.ketchup.backend.helper.DisplayMessage;
+import com.ducksabervn.projects.ketchup.backend.auth.Credential;
+import com.ducksabervn.projects.ketchup.backend.auth.CredentialRepository;
+import com.ducksabervn.projects.ketchup.backend.ui.DisplayMessage;
 
 import javax.swing.*;
 import java.awt.*;
