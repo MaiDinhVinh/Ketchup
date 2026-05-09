4e1b42857f6c21a5dc73ec22db8573df9e63fcca
Nguyen Dinh Quy <25quy.nd@vinuni.edu.vn>
2026-05-09 13:13:11 +0700

    Update BookingCsvIO.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/BookingCsvIO.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/BookingCsvIO.java
@@ -11,12 +11,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class BookingCsvIO implements CsvIO<String, Booking>{
+public class BookingCsvIO implements FilteredCsvIO<String, Booking>{
 
     private static BookingCsvIO IO;
 
@@ -28,11 +26,6 @@ public class BookingCsvIO implements CsvIO<String, Booking>{
             BookingCsvIO.IO = new BookingCsvIO();
         }
         return BookingCsvIO.IO;
-    }
-
-    @Override
-    public LinkedHashMap<String, Booking> readCsvFile() {
-        throw new UnsupportedOperationException("Unsupported Operation");
     }
 
     @Override
@@ -48,14 +41,14 @@ public class BookingCsvIO implements CsvIO<String, Booking>{
     //why we need userEmail ? Because normal/admin user can't see other user's private data
     private LinkedHashMap<String, Booking> readBookingCsv(String userEmail){
         try{
-            List<String> allBookings = Files.readAllLines(FileSystemInitializer.getBOOKINGS());
+            List<String> allBookings = Files.readAllLines(CsvPersistable.BOOKINGS);
             allBookings.remove(0);
             LinkedHashMap<String, Booking> bookings = new LinkedHashMap<>();
             for(String b: allBookings){
                 String[] split = b.split(";");
                 if(split[0].equals(userEmail)){
                     LocalDateTime showtime = LocalDateTime.parse(split[3], Movie.getDatetimeFormat());
-                    ArrayList<String> chosenSeats = new ArrayList<>(Arrays.asList(split[4].split(",")));
+                    HashSet<String> chosenSeats = new HashSet<>(Arrays.asList(split[4].split(",")));
                     int totalPrice = Integer.parseInt(split[5]);
                     boolean isProcessed = Boolean.parseBoolean(split[6]);
                     bookings.put(split[1], new Booking(split[0],
@@ -74,12 +67,12 @@ public class BookingCsvIO implements CsvIO<String, Booking>{
 
     private void updateBookingCsv(){
         LinkedHashMap<String, Booking> allBookings = BookingRepository.getBookings();
-        try(BufferedWriter bw3 = new BufferedWriter(new FileWriter(FileSystemInitializer.getBOOKINGS().toFile(), true))){
+        try(BufferedWriter bw3 = new BufferedWriter(new FileWriter(CsvPersistable.BOOKINGS.toFile(), true))){
             for(Booking b: allBookings.values()){
                 if(!b.isProcessed()){
                     b.setProcessed(true);
                     bw3.newLine();
-                    bw3.write(BookingRepository.generateDataAsString(b));
+                    bw3.write(generateDataAsString(b));
                 }
             }
         }catch(IOException e){
@@ -88,5 +81,14 @@ public class BookingCsvIO implements CsvIO<String, Booking>{
             DisplayMessage.displayError(AdminMovieListUI.getAdminMovieListUI().getMainFrame(),
                     e.getMessage());
         }
+    }
+
+    public static String generateDataAsString(Booking b){
+        String selectedSeatIds = b.getChosenSeats().stream().
+                collect(Collectors.joining(","));
+        String data = "%s;%s;%s;%s;%s;%d;%b".formatted(b.getBookingEmail(),
+                b.getBookingId(), b.getMovieId(), b.getShowtime().format(Movie.getDatetimeFormat()),
+                selectedSeatIds, b.getTotalPrice(), b.isProcessed());
+        return data;
     }
 }
