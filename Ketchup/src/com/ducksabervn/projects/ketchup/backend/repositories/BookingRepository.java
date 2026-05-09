9a438c182bb4e68bb26435fbad1dff832ada192a
Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>
2026-05-09 14:10:49 +0700

    Create BookingRepository.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

---
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/repositories/BookingRepository.java
@@ -0,0 +1,53 @@
+package com.ducksabervn.projects.ketchup.backend.repositories;
+
+import com.ducksabervn.projects.ketchup.backend.model.Booking;
+import com.ducksabervn.projects.ketchup.backend.model.Movie;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class BookingRepository {
+    //this motherfucker will maintain the insert input unlike HashMap, bro i want to kms
+    private static LinkedHashMap<String, Booking> bookings;
+
+    public static LinkedHashMap<String, Booking> getBookings() {
+        return bookings;
+    }
+
+    public static void setBookings(LinkedHashMap<String, Booking> movies) {
+        BookingRepository.bookings = movies;
+    }
+
+    public static Booking addBooking(String email,
+                                     String movieId,
+                                     String showtime,
+                                     HashSet<String> selectedSeats,
+                                     int totalPrice) {
+        String bookingId = UUID.randomUUID().toString();
+        LocalDateTime st = LocalDateTime.parse(showtime, Movie.getDatetimeFormat());
+        Booking b = new Booking(email,
+                bookingId, movieId, st, selectedSeats, totalPrice, false);
+        BookingRepository.bookings.put(bookingId, b);
+        return b;
+    }
+
+    public static ArrayList<Booking> searchBookings(String information){
+        ArrayList<Booking> arr = new ArrayList<>();
+        if(BookingRepository.bookings.containsKey(information)){
+            arr.add(BookingRepository.bookings.get(information)); //searching by id
+        }else{
+            for(Booking b: BookingRepository.bookings.values()){
+                if(MovieRepository.getMovies().get(b.getMovieId()).getTitle().equals(information) ||
+                b.getShowtime().format(Movie.getDatetimeFormat()).equals(information) ||
+                Integer.toString(b.getTotalPrice()).equals(information)){
+                    arr.add(b);
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static int calculateTotalPrice(HashSet<String> chosenSeats, int pricePerSeat){
+        return chosenSeats.size() * pricePerSeat;
+    }
+}
