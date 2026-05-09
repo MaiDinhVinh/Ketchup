e5ba020a9214e57998f510925c1d5db02484dbda
Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>
2026-05-09 14:09:48 +0700

    Update CsvIO.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/CsvIO.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/CsvIO.java
@@ -1,8 +1,8 @@
 package com.ducksabervn.projects.ketchup.backend.io;
 
-import java.nio.file.Path;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 interface CsvIO<U, V> extends CsvPersistable{
-    LinkedHashMap<U, V> readCsvFile();
+    LinkedHashMap<U, V> readCsvFile() throws IOException;
 }
