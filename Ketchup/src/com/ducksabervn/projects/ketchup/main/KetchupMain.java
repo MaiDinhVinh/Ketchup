4fc56533065608f9b336f8872b785d5da978fd3e
Nguyen Dinh Quy <25quy.nd@vinuni.edu.vn>
2026-05-09 14:12:08 +0700

    Update KetchupMain.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/main/KetchupMain.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/main/KetchupMain.java
@@ -1,13 +1,16 @@
 package com.ducksabervn.projects.ketchup.main;
 
 import com.ducksabervn.projects.ketchup.backend.io.CredentialCsvIO;
-import com.ducksabervn.projects.ketchup.backend.movie.MovieRepository;
-import com.ducksabervn.projects.ketchup.backend.auth.CredentialRepository;
+import com.ducksabervn.projects.ketchup.backend.repositories.MovieRepository;
+import com.ducksabervn.projects.ketchup.backend.repositories.CredentialRepository;
 import com.ducksabervn.projects.ketchup.backend.io.FileSystemInitializer;
 import com.ducksabervn.projects.ketchup.backend.io.MovieCsvIO;
+import com.ducksabervn.projects.ketchup.frontend.util.DisplayMessage;
+import com.ducksabervn.projects.ketchup.frontend.AdminMovieListUI;
 import com.ducksabervn.projects.ketchup.frontend.LoginUI;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class KetchupMain {
     public static void main(String[] args) {
@@ -16,9 +19,17 @@ public class KetchupMain {
         }catch(ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e){
             e.printStackTrace();
         }
-        FileSystemInitializer.initalize();
-        MovieRepository.setMovies(MovieCsvIO.getIO().readCsvFile());
-        CredentialRepository.setCredentials(CredentialCsvIO.getIO().readCsvFile());
+
+        try{
+            FileSystemInitializer.initalize();
+            MovieRepository.setMovies(MovieCsvIO.getIO().readCsvFile());
+            CredentialRepository.setCredentials(CredentialCsvIO.getIO().readCsvFile());
+        }catch(IOException e){
+            //I still cant figure out for which JFrame will responsible to display the
+            //exception string, but this will work as a fallback for now
+            DisplayMessage.displayError(AdminMovieListUI.getAdminMovieListUI().getMainFrame(),
+                    e.getMessage());
+        }
         SwingUtilities.invokeLater(LoginUI::initialize);
     }
 }
