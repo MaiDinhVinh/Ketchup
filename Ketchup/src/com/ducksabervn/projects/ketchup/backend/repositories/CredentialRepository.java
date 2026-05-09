f7cb545d7a810c1c6c4a3bca1ad875631a10f749
Hoang Duc Phat <25phat.hd@vinuni.edu.vn>
2026-05-09 14:10:55 +0700

    Create CredentialRepository.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

---
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/repositories/CredentialRepository.java
@@ -0,0 +1,35 @@
+package com.ducksabervn.projects.ketchup.backend.repositories;
+
+import com.ducksabervn.projects.ketchup.backend.model.Credential;
+
+import java.util.LinkedHashMap;
+
+public class CredentialRepository {
+    private static LinkedHashMap<String, Credential> credentials;
+
+    public static LinkedHashMap<String, Credential> getCredentials() {
+        return CredentialRepository.credentials;
+    }
+
+    public static void setCredentials(LinkedHashMap<String, Credential> credentials) {
+        CredentialRepository.credentials = credentials;
+    }
+
+    public static boolean verifyCredential(String email, String password){
+        return CredentialRepository.credentials.containsKey(email) &&
+                CredentialRepository.credentials.get(email).getPassword().equals(password);
+    }
+
+    public static Credential getUser(String email){
+        return CredentialRepository.credentials.get(email);
+    }
+
+    public static boolean register(String username, String email, String password, boolean isAdmin){
+        if(CredentialRepository.credentials.containsKey(email)){
+            return false;
+        }else{
+            CredentialRepository.credentials.put(email, new Credential(username, email, password, isAdmin));
+            return true;
+        }
+    }
+}
