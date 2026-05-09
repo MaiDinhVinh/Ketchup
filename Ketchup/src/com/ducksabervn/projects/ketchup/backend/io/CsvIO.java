d43506a702c139cb664892782489a782de4a2598
Hoang Duc Phat <25phat.hd@vinuni.edu.vn>
2026-05-09 13:13:26 +0700

    Update CsvIO.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/CsvIO.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/CsvIO.java
@@ -3,16 +3,6 @@ package com.ducksabervn.projects.ketchup.backend.io;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 
-interface CsvIO<U, V>{
-    Path APP_DIRECTORY = Path.of(System.getProperty("user.home"), "Ketchup");
-    Path USER_CREDENTIALS = Path.of(System.getProperty("user.home"), "Ketchup",
-            "USER_CREDENTIALS.csv");
-    Path MOVIES = Path.of(System.getProperty("user.home"), "Ketchup",
-            "MOVIES.csv");
-    Path BOOKINGS = Path.of(System.getProperty("user.home"), "Ketchup",
-            "BOOKINGS.csv");
-
+interface CsvIO<U, V> extends CsvPersistable{
     LinkedHashMap<U, V> readCsvFile();
-    LinkedHashMap<U, V> readCsvFile(String requiredInformation);
-    void updateLatestData();
 }
