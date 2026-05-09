d2abb333f2aa314f19679e9364669ebe528f7a73
Hoang Duc Phat <25phat.hd@vinuni.edu.vn>
2026-05-09 14:12:01 +0700

    Create DisplayMessage.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

---
+++ Ketchup/src/com/ducksabervn/projects/ketchup/frontend/util/DisplayMessage.java
@@ -0,0 +1,34 @@
+package com.ducksabervn.projects.ketchup.frontend.util;
+
+import javax.swing.*;
+import java.awt.*;
+
+public final class DisplayMessage {
+    public static void displayError(Component component, String message){
+        JOptionPane.showMessageDialog(
+                component,
+                message,
+                "Ketchup",
+                JOptionPane.ERROR_MESSAGE
+        );
+    }
+
+    public static boolean displayConfirmationDialog(Component component,
+                                                    String message,
+                                                    String title){
+        return JOptionPane.showConfirmDialog(component,
+                message,
+                title,
+                JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+    }
+
+    public static void displayWarning(Component component,
+                                      String message){
+        JOptionPane.showMessageDialog(component, message, "Ketchup", JOptionPane.WARNING_MESSAGE);
+    }
+
+    public static void displayInformation(Component component,
+                                          String message){
+        JOptionPane.showMessageDialog(component, message, "Ketchup", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
