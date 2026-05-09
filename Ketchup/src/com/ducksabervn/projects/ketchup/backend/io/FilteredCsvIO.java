97e1a84b2698b707ca3e736e05007fdc2dd51012
Nguyen Trong Khoi Nguyen <25nguyen.ntk@vinuni.edu.vn>
2026-05-09 14:10:05 +0700

    Update FilteredCsvIO.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FilteredCsvIO.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FilteredCsvIO.java
@@ -1,8 +1,8 @@
 package com.ducksabervn.projects.ketchup.backend.io;
 
-import java.nio.file.Path;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 
 interface FilteredCsvIO<U, V> extends CsvPersistable{
-    LinkedHashMap<U, V> readCsvFile(String requiredInformation);
+    LinkedHashMap<U, V> readCsvFile(String requiredInformation) throws IOException;
 }
