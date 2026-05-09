1a8f0fb34a31a12d6e1ea520f5aeffe6c525be23
Tran Phan Anh <25anh.tp@vinuni.edu.vn>
2026-05-09 14:09:59 +0700

    Update FileSystemInitializer.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FileSystemInitializer.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FileSystemInitializer.java
@@ -1,35 +1,28 @@
 package com.ducksabervn.projects.ketchup.backend.io;
 
-import com.ducksabervn.projects.ketchup.backend.ui.DisplayMessage;
-import com.ducksabervn.projects.ketchup.frontend.AdminMovieListUI;
-
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 public final class FileSystemInitializer {
-    public static void initalize(){
+    public static void initalize() throws IOException{
         try{
-            if(!Files.exists(CsvPersistable.APP_DIRECTORY)) {
-                Files.createDirectory(CsvPersistable.APP_DIRECTORY);
+            if(!Files.exists(AppPath.APP_DIRECTORY.getAppPath())) {
+                Files.createDirectory(AppPath.APP_DIRECTORY.getAppPath());
             }
-            if(!Files.exists(CsvPersistable.USER_CREDENTIALS)){
-                Files.createFile(CsvPersistable.USER_CREDENTIALS);
-                Files.writeString(CsvPersistable.USER_CREDENTIALS, "USERNAME;EMAIL;PASSWORD;IS_ADMIN");
+            if(!Files.exists(AppPath.USER_CREDENTIALS.getAppPath())){
+                Files.createFile(AppPath.USER_CREDENTIALS.getAppPath());
+                Files.writeString(AppPath.USER_CREDENTIALS.getAppPath(), "USERNAME;EMAIL;PASSWORD;IS_ADMIN");
             }
-            if(!Files.exists(CsvPersistable.MOVIES)){
-                Files.createFile(CsvPersistable.MOVIES);
-                Files.writeString(CsvPersistable.MOVIES, "MOVIEID;TITLE;GENRE;DURATION;RATING;SHOWTIME;SEAT;SPRICE");
+            if(!Files.exists(AppPath.MOVIES.getAppPath())){
+                Files.createFile(AppPath.MOVIES.getAppPath());
+                Files.writeString(AppPath.MOVIES.getAppPath(), "MOVIEID;TITLE;GENRE;DURATION;RATING;SHOWTIME;SEAT;SPRICE");
             }
-            if(!Files.exists(CsvPersistable.BOOKINGS)){
-                Files.createFile(CsvPersistable.BOOKINGS);
-                Files.writeString(CsvPersistable.BOOKINGS, "EMAIL;BOOKINGID;MOVIEID;SHOWTIME;SEATS;TOTAL_PRICE");
+            if(!Files.exists(AppPath.BOOKINGS.getAppPath())){
+                Files.createFile(AppPath.BOOKINGS.getAppPath());
+                Files.writeString(AppPath.BOOKINGS.getAppPath(), "EMAIL;BOOKINGID;MOVIEID;SHOWTIME;SEATS;TOTAL_PRICE");
             }
         }catch (IOException e){
-            //I still cant figure out for which JFrame will responsible to display the
-            //exception string, but this will work as a fallback for now
-            DisplayMessage.displayError(AdminMovieListUI.getAdminMovieListUI().getMainFrame(),
-                    e.getMessage());
+            throw e;
         }
     }
 }
