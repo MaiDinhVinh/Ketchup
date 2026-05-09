823e9e341d59a11dfc57ea61a7ff15aefb578a71
Nguyen The Khoi Nguyen <25nguyen.ntk@vinuni.edu.vn>
2026-05-09 13:13:40 +0700

    Update FileSystemInitializer.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

--- Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FileSystemInitializer.java
+++ Ketchup/src/com/ducksabervn/projects/ketchup/backend/io/FileSystemInitializer.java
@@ -8,38 +8,22 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public final class FileSystemInitializer {
-    static Path getAppDirectory() {
-        return CsvIO.APP_DIRECTORY;
-    }
-
-    static Path getBOOKINGS() {
-        return CsvIO.BOOKINGS;
-    }
-
-    static Path getMOVIES() {
-        return CsvIO.MOVIES;
-    }
-
-    static Path getUserCredentials() {
-        return CsvIO.USER_CREDENTIALS;
-    }
-
     public static void initalize(){
         try{
-            if(!Files.exists(CsvIO.APP_DIRECTORY)) {
-                Files.createDirectory(CsvIO.APP_DIRECTORY);
+            if(!Files.exists(CsvPersistable.APP_DIRECTORY)) {
+                Files.createDirectory(CsvPersistable.APP_DIRECTORY);
             }
-            if(!Files.exists(CsvIO.USER_CREDENTIALS)){
-                Files.createFile(CsvIO.USER_CREDENTIALS);
-                Files.writeString(CsvIO.USER_CREDENTIALS, "USERNAME;EMAIL;PASSWORD;IS_ADMIN");
+            if(!Files.exists(CsvPersistable.USER_CREDENTIALS)){
+                Files.createFile(CsvPersistable.USER_CREDENTIALS);
+                Files.writeString(CsvPersistable.USER_CREDENTIALS, "USERNAME;EMAIL;PASSWORD;IS_ADMIN");
             }
-            if(!Files.exists(CsvIO.MOVIES)){
-                Files.createFile(CsvIO.MOVIES);
-                Files.writeString(CsvIO.MOVIES, "MOVIEID;TITLE;GENRE;DURATION;RATING;SHOWTIME;SEAT;SPRICE");
+            if(!Files.exists(CsvPersistable.MOVIES)){
+                Files.createFile(CsvPersistable.MOVIES);
+                Files.writeString(CsvPersistable.MOVIES, "MOVIEID;TITLE;GENRE;DURATION;RATING;SHOWTIME;SEAT;SPRICE");
             }
-            if(!Files.exists(CsvIO.BOOKINGS)){
-                Files.createFile(CsvIO.BOOKINGS);
-                Files.writeString(CsvIO.BOOKINGS, "EMAIL;BOOKINGID;MOVIEID;SHOWTIME;SEATS;TOTAL_PRICE");
+            if(!Files.exists(CsvPersistable.BOOKINGS)){
+                Files.createFile(CsvPersistable.BOOKINGS);
+                Files.writeString(CsvPersistable.BOOKINGS, "EMAIL;BOOKINGID;MOVIEID;SHOWTIME;SEATS;TOTAL_PRICE");
             }
         }catch (IOException e){
             //I still cant figure out for which JFrame will responsible to display the
