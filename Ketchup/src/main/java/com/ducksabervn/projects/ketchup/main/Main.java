4e73ac969c8d51facb8d35a9b97d5916aa96d751
Hoang Duc Phat <25phat.hd@vinuni.edu.vn>
2026-05-22 09:07:15 +0700

    Create Main.java
    Commit verified and tested by: Tran Phan Anh <25anh.tp@vinuni.edu.vn> and Mai Dinh Vinh <25vinh.md@vinuni.edu.vn>

---
+++ Ketchup/src/main/java/com/ducksabervn/projects/ketchup/main/Main.java
@@ -0,0 +1,8 @@
+package com.ducksabervn.projects.ketchup.main;
+
+//fake main class to bypass JVM JavaFX check
+public class Main {
+    public static void main(String[] args) {
+        KetchupMain.main(args);
+    }
+}
